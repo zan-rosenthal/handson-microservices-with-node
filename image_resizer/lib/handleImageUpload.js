ffd6265e530d3d4147370f5ede6d7d8f47e6877e
zan-rosenthal <zan.rosenthal@gmail.com>
2019-01-19 17:51:25 -0600

    Handle image upload

---
+++ image_resizer/lib/handleImageUpload.js
@@ -0,0 +1,27 @@
+import path from 'path'
+import fs from 'fs'
+
+export default (req, res) =>  {
+  console.log('HANDLING UPLOAD', req.body)
+  let image = req.params.image.toLowerCase()
+
+  if (!image.match(/\.(png|jpg)$/)) {
+    return res.status(403).end()
+  }
+
+  const length = req.body.length
+  const fd = fs.createWriteStream(
+    path.join(__dirname, "uploads", image), {
+      flages: "w+",
+      encoding: "binary"
+    }
+  )
+
+  // fd.write(req.body)
+  fd.end(req.body)
+
+  fd.on("close", () => {
+    res.send({ status: "ok", size: length });
+  })
+}
+
--- image_resizer/lib/index.js
+++ image_resizer/lib/index.js
@@ -1,9 +1,22 @@
 import express from "express";
-import handleImageRequest from './handleImageRequest'
+import handleResize from './handleResize'
+import handleImageUpload from './handleImageUpload'
+import bodyParser from "body-parser";
+import morgan from 'morgan' 
 
 const app = express();
+morgan('combined')
 
-app.get(/\/thumbnail\.(jpg|png)/, handleImageRequest);
+// Routes
+app.get(/\/thumbnail\.(jpg|png)/, handleResize);
+app.post(
+  '/uploads/:image',
+  bodyParser.raw({
+    limit: "10mb",
+    type: "image/*"
+  }),
+  handleImageUpload
+)
 
 app.listen(3000, () => {
   console.log("ready");
